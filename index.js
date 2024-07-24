6a5cfc7ab93dc3fd5005a1057e8062eb856e31a5
Pavan0896 <pavankumar170896@gmail.com>
2024-07-24 10:33:07 +0530

    feat:backend of hm.com having features of filtering, sorting, search

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.use(express.json());
 app.use("/products", prodRouter);
 app.use("/user", userRouter);
 
+app.get("/", (req, res) => {
+  res.status(200).send("Health check is fine");
+});
+
 app.listen(port, async () => {
   await connection;
   console.log(`Server is running on ${port} and DB is connected`);
