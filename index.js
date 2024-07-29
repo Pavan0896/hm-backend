d59167843e199dddf3b8c429140249e7ea68a0f2
Pavan0896 <pavankumar170896@gmail.com>
2024-07-29 22:17:54 +0530

    Mofidied backend

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const express = require("express");
 const connection = require("./config/db");
 const prodRouter = require("./routes/product.routes");
 const userRouter = require("./routes/users.router");
-const cors = require("cors")
+const path = require("path");
+const cors = require("cors");
 require("dotenv").config();
 
 const port = process.env.PORT;
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use("/products", prodRouter);
 app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.status(200).send("Health check is fine");
+});
+
+app.use(express.static(path.join(__dirname, "Frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "Frontend/dist", "index.html"));
 });
 
 app.listen(port, async () => {
