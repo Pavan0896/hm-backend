53d94f1632e132a233b24c3339430b24419d65e4
Pavan0896 <pavankumar170896@gmail.com>
2024-07-29 23:00:41 +0530

    Latest backend commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const connection = require("./config/db");
 const prodRouter = require("./routes/product.routes");
 const userRouter = require("./routes/users.router");
-const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -15,12 +14,6 @@ app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.status(200).send("Health check is fine");
-});
-
-app.use(express.static(path.join(__dirname, "Frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "Frontend/dist", "index.html"));
 });
 
 app.listen(port, async () => {
